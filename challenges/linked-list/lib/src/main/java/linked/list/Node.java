477986690256bef809fc700c08abf79f80aa3b8c
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-07-05 22:43:21 +0300

    Finished code Challenge 05

---
+++ challenges/linked-list/lib/src/main/java/linked/list/Node.java
@@ -0,0 +1,19 @@
+package linked.list;
+
+public class Node {
+public int value;
+Node next;
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
